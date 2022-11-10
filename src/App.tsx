4b37570ae56a8690ad48996ed61c53be1dfca861
Fusibu <ollerexpositomartin@gmail.com>
2022-11-10 23:13:32 +0100

    Fix-bug-windups

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,14 @@ function App() {
   );
 
   return (
-    <div className='flex flex-row absolute h-full w-screen '>
+    <div className='flex flex-row absolute min-w-full h-full'>
       <div className='bg-gray-800 w-2/5' >
         <Logo />
         <Form />
       </div>
-      <div className='bg-img-login w-full '>
-        <div className='relative top-1/4 left-1/3 max-w-lg'>
-          <p className='text-9xl text-gray-200 font-bebasNeu '>{text}</p>
+      <div className='bg-img-login w-full'>
+        <div className='w-full top-1/3 left-1/2 fixed text-center'>
+          <p className='text-7xl text-gray-200 font-bebasNeu max-w-lg '>{text}</p>
         </div>
       </div>
     </div>
