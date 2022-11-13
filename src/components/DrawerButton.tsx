647acc9847c6f53ec2626160a5c50f6632b7e415
Fusibu <ollerexpositomartin@gmail.com>
2022-11-13 21:50:35 +0100

    cleaning code

--- src/components/DrawerButton.tsx
+++ src/components/DrawerButton.tsx
@@ -1,44 +1,47 @@
 import React, { useEffect, useState } from "react";
 import { useIndex } from "../customHooks/useIndex";
-import {IconType,IconBase} from 'react-icons/lib'
+import { IconType, IconBase } from "react-icons/lib";
 import { GenIcon } from "react-icons";
 import { BsFillHouseFill, BsFilm, BsTvFill } from "react-icons/bs";
 
 type DrawerButtonConf = {
-    icon:IconType,
-    name:string
-}
+  icon: IconType;
+  name: string;
+};
 
-let ButtonCounter =  {
-    counter: -1
-}
+let ButtonCounter = {
+  counter: -1,
+};
 
-class ButtonState {
-    buttonPosition:number
-    constructor(){
-        this.buttonPosition = 0
-    }
-}
+function DrawerButton({ icon, name }: DrawerButtonConf) {
+  const { index, setPositionIndex } = useIndex();
 
-function DrawerButton ({icon,name}:DrawerButtonConf){
-    const {index,setPositionIndex} = useIndex()
+  useEffect(() => {
+    ButtonCounter.counter = -1;
+  });
+  ButtonCounter.counter++;
 
-    useEffect(()=>{
-        ButtonCounter.counter = -1
-    })
-    ButtonCounter.counter++
+  let buttonPosition = ButtonCounter.counter;
 
-    let buttonState = new ButtonState()
-    buttonState.buttonPosition = ButtonCounter.counter
-   
-   
-    return(
-        <li>
-        <a onClick={()=>{setPositionIndex(buttonState.buttonPosition)}} className={`${index==buttonState.buttonPosition?'text-primary border-r-4  rounded-sm':''} border-primary font-Roboto  rounded-sm`}>
-            {React.createElement(icon, { width: 15, height: 15, color:`${index==buttonState.buttonPosition?'#6419E6':''}`})}
-            {name}</a>
-      </li>
-    )
+  return (
+    <li>
+      <a
+        onClick={() => {
+          setPositionIndex(buttonPosition);
+        }}
+        className={`${
+          index == buttonPosition ? "text-primary border-r-4  rounded-sm" : ""
+        } border-primary font-Roboto  rounded-sm`}
+      >
+        {React.createElement(icon, {
+          width: 15,
+          height: 15,
+          color: `${index == buttonPosition ? "#6419E6" : ""}`,
+        })}
+        {name}
+      </a>
+    </li>
+  );
 }
 
 export default DrawerButton;
--- src/components/HeroButton.tsx
+++ src/components/HeroButton.tsx
@@ -2,37 +2,37 @@ import React, { useEffect } from "react";
 import { useIndex } from "../customHooks/useIndex";
 
 type ButtonConf = {
-    name:string
-}
+  name: string;
+};
 
-let ButtonCounter =  {
-    counter: -1
-}
+let ButtonCounter = {
+  counter: -1,
+};
 
-class ButtonState {
-    buttonPosition:number
-    constructor(){
-        this.buttonPosition = 0
-    }
-}
+function HeroButton({ name }: ButtonConf) {
+  const { index, setPositionIndex } = useIndex();
 
-function HeroButton ({name}:ButtonConf){
-    const {index,setPositionIndex} = useIndex()
+  useEffect(() => {
+    ButtonCounter.counter = -1;
+  });
+  ButtonCounter.counter++;
 
-    useEffect(()=>{
-        ButtonCounter.counter = -1
-    })
-    ButtonCounter.counter++
+  let buttonPosition = ButtonCounter.counter;
 
-    let buttonState = new ButtonState()
-    buttonState.buttonPosition = ButtonCounter.counter
-   
-   
-    return(
-        <li>
-        <a onClick={()=>{setPositionIndex(buttonState.buttonPosition)}} className={`${index==buttonState.buttonPosition?'text-white':''} font-Roboto hover:bg-transparent`}>{name}</a>
-      </li>
-    )
+  return (
+    <li>
+      <a
+        onClick={() => {
+          setPositionIndex(buttonPosition);
+        }}
+        className={`${
+          index == buttonPosition ? "text-white" : ""
+        } font-Roboto hover:bg-transparent`}
+      >
+        {name}
+      </a>
+    </li>
+  );
 }
 
 export default HeroButton;
